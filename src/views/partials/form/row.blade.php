ae0dfbf7166537994bb2b7ad4eba4cfc7bb8566b
Marko Šamec <marko.samec@am2studio.hr>
2016-03-16 09:30:55 +0100

    Label missing

--- src/views/partials/form/row.blade.php
+++ src/views/partials/form/row.blade.php
@@ -1,5 +1,7 @@
 <div class="card-table-row">
-    <span class="card-table-cell @if(isset($options['labelClass'])) {{ $options['labelClass'] }} @else fixed25 @endif"></span>
+    <span class="card-table-cell @if(isset($options['labelClass'])) {{ $options['labelClass'] }} @else fixed25 @endif">
+        {{ $label }}
+    </span>
     <div class="card-table-cell">
         {!! $element !!}
     </div>
