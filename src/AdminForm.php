0e9c58a8e505fed15bf762cb89ca8359d62766a1
Marko Šamec <marko.samec@am2studio.hr>
2016-03-17 13:59:26 +0100

    Footer button

--- src/AdminForm.php
+++ src/AdminForm.php
@@ -72,6 +72,20 @@ class AdminForm extends FormBuilder
         return View::make('adminForm::submitRow', compact('element', 'options'));
     }
 
+    public function footerButtonRight($label, $options)
+    {
+        $options['class'] .= ' right btn btn--secondary';
+
+        return View::make('adminForm::footerButton', compact('label', 'options'));
+    }
+
+    public function footerButtonLeft($label, $options)
+    {
+        $options['class'] .= ' left btn btn--secondary';
+
+        return View::make('adminForm::footerButton', compact('label', 'options'));
+    }
+
     private function placeholder($options, $name)
     {
         if (!isset($options['placeholder'])) {
---
+++ src/views/partials/form/footerButton.blade.php
@@ -0,0 +1,1 @@
+<button class="{{ $options['class'] }}">{{ $label }}</button>
