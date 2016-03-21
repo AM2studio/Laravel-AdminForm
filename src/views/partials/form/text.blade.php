766356c136d2b4f8f16302727b58e763d911f591
Marko Šamec <marko.samec@am2studio.hr>
2016-03-21 14:05:36 +0100

    Remove HC no-inline-edit

--- src/views/partials/form/text.blade.php
+++ src/views/partials/form/text.blade.php
@@ -1,5 +1,5 @@
 <div class="card-form">
-    <fieldset class="no-inline-edit">
+    <fieldset>
         {!! Form::text($name, $value, $options) !!}
         <button data-js="submit-field" type="submit"><i class="fa fa-check"></i></button>
         <i class="fieldset-overlay" data-js="focus-on-field"></i>
