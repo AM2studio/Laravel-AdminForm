bbad51f9e2ed434a12b9f9a12ef74aa3ae58261b
Boris Vidošević <boris.vidosevic@am2studio.hr>
2016-06-06 13:15:43 +0200

    Updated class fixed25 to fixed250

--- src/views/partials/form/row.blade.php
+++ src/views/partials/form/row.blade.php
@@ -1,5 +1,5 @@
 <div class="card-table-row @if(isset($options['rowClass'])) {{ $options['rowClass'] }} @endif">
-    <span class="card-table-cell @if(isset($options['labelClass'])) {{ $options['labelClass'] }} @else fixed25 @endif">
+    <span class="card-table-cell @if(isset($options['labelClass'])) {{ $options['labelClass'] }} @else fixed250 @endif">
         {{ $label }}
     </span>
     <div class="card-table-cell">
