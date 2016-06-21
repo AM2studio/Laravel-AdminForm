f5440ba50d2f70c870b4d0f80f7c85e8c95bdf4d
Filip Horvat <fico7489@gmail.com>
2016-06-21 11:58:30 +0200

    fix(form): add fa before icon

--- src/views/partials/form/submit.blade.php
+++ src/views/partials/form/submit.blade.php
@@ -3,5 +3,5 @@
 	@if(isset($options['form'])) form="{{ $options['form'] }}" @endif
 	@if(isset($options['id'])) id="{{ $options['id'] }}" @endif
 	type="<?php echo isset($options['type']) ? $options['type'] : 'submit' ?>">
-	@if(isset($options['icon'])) <span class="{{ $options['icon'] }}"></span> @endif <span>{{ $value }}</span>
+	@if(isset($options['icon'])) <span class="fa {{ $options['icon'] }}"></span> @endif <span>{{ $value }}</span>
 </button>
