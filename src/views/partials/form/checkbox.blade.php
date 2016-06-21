53e13de149146713d4d65824a2496d8405ce4b0e
Filip Horvat <fico7489@gmail.com>
2016-06-21 11:43:44 +0200

    fix(checkbox): form attribute error .

--- src/views/partials/form/checkbox.blade.php
+++ src/views/partials/form/checkbox.blade.php
@@ -1,7 +1,13 @@
+<?php
+	$optionsForm = [];
+	if(isset($options['form'])){
+		$optionsForm['form'] = $options['form'];
+	}
+?>
 <div class="card-form">
     <fieldset class="checkbox-toggle">
         @if(!isset($options['ignore-hidden']))
-            {!! Form::hidden($name, 0, $options) !!}
+            {!! Form::hidden($name, 0, $optionsForm) !!}
         @endif
         @if( isset($options['repeater']) )
             {!! Form::checkbox($name, $value, $checked, array_merge($options, ['data-repeater-randomize'=>'id'] ) ) !!}
