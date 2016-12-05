c7a263ba401d484c05c7551fb87d53eadabd03a2
Filip Horvat <fico7489@gmail.com>
2016-12-05 16:34:05 +0100

    fix

--- src/views/partials/form/text.blade.php
+++ src/views/partials/form/text.blade.php
@@ -1,7 +1,9 @@
 <div class="card-form">
-    <fieldset>
-        {!! Form::text($name, $value, $options) !!}
-        <button data-js="submit-field" type="submit"><i class="fa fa-check"></i></button>
-        <i class="fieldset-overlay" data-js="focus-on-field"></i>
-    </fieldset>
+   <fieldset>
+       {!! Form::text($name, $value, $options) !!}
+       @if( !isset($options['hideSubmitField']) )
+       <button data-js="submit-field" type="submit"><i class="fa fa-check"></i></button>
+       <i class="fieldset-overlay" data-js="focus-on-field"></i>
+       @endif
+   </fieldset>
 </div>
