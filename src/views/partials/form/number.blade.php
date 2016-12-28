f30f53289e9fb8298370254747a0be453869fdfa
Filip Horvat <fico7489@gmail.com>
2016-12-28 11:44:11 +0100

    update

--- src/views/partials/form/number.blade.php
+++ src/views/partials/form/number.blade.php
@@ -1,7 +1,11 @@
 <div class="card-form">
-    <fieldset>
-        {!! Form::number($name, $value, $options) !!}
-        <button data-js="submit-field" type="submit"><i class="fa fa-check"></i></button>
-        <i class="fieldset-overlay" data-js="focus-on-field"></i>
-    </fieldset>
+   <fieldset>
+       <div class="input-group">
+           @if( isset($options['icon_prefix']) ) <div class="input-group__addon"><span class="fa {{ $options['icon_prefix'] }}"></span></div>@endif
+            {!! Form::number($name, $value, $options) !!}
+            <button data-js="submit-field" type="submit"><i class="fa fa-check"></i></button>
+            <i class="fieldset-overlay" data-js="focus-on-field"></i>
+            @if( isset($options['icon_suffix']) ) <div class="input-group__addon"><span class="fa {{ $options['icon_suffix'] }}"></span></div>@endif
+        </div>
+   </fieldset>
 </div>
