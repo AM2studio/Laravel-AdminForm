a00d3ad67d34f219e7d22e25c6ddedd7043740ec
Filip Horvat <fico7489@gmail.com>
2016-12-28 12:46:45 +0100

    update2

--- src/views/partials/form/number.blade.php
+++ src/views/partials/form/number.blade.php
@@ -1,8 +1,11 @@
 <div class="card-form">
    <fieldset>
        <div class="input-group">
+           @php 
+               $valueFormated = ( isset($options['forceFloat']) && $options['forceFloat']==true ) ? floatval($value) : $value; 
+           @endphp
            @if( isset($options['icon_prefix']) ) <div class="input-group__addon"><span class="fa {{ $options['icon_prefix'] }}"></span></div>@endif
-            {!! Form::number($name, $value, $options) !!}
+            {!! Form::number($name, $valueFormated, $options) !!}
             <button data-js="submit-field" type="submit"><i class="fa fa-check"></i></button>
             <i class="fieldset-overlay" data-js="focus-on-field"></i>
             @if( isset($options['icon_suffix']) ) <div class="input-group__addon"><span class="fa {{ $options['icon_suffix'] }}"></span></div>@endif
